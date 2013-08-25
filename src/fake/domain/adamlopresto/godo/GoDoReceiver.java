20caf66beaf903930d878cc1db7602552f53df98
Adam Lopresto <adamlopresto@gmail.com>
2013-08-25 12:07:33 -0500

    More receiver cleanup

--- src/fake/domain/adamlopresto/godo/GoDoReceiver.java
+++ src/fake/domain/adamlopresto/godo/GoDoReceiver.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import fake.domain.adamlopresto.godo.db.ContextsTable;
 
 public class GoDoReceiver extends BroadcastReceiver {
@@ -15,36 +16,44 @@ public class GoDoReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		
-		Bundle extras = intent.getExtras();
-		ContentResolver res = context.getContentResolver();
-		ContentValues values = new ContentValues(1);
-		String where = ContextsTable.COLUMN_NAME+"=?";
-		String[] selectionArgs = new String[1];
-		
-		int max = 4;
-		if (extras != null){
-			String[] deactivate = extras.getStringArray("deactivate");
-			if (deactivate != null){
-				values.put(ContextsTable.COLUMN_ACTIVE, 0);
-				for (String d : deactivate){
-					selectionArgs[0] = d;
-					res.update(GoDoContentProvider.CONTEXTS_URI, values, where, selectionArgs);
-				}
-			}
+		String action = intent.getAction();
+		if (action == null || 
+				"com.twofortyfouram.locale.intent.action.FIRE_SETTING".equals(action) ||
+				"android.intent.action.BOOT_COMPLETED".equals(action)){
+			Bundle extras = intent.getExtras();
+			ContentResolver res = context.getContentResolver();
+			ContentValues values = new ContentValues(1);
+			String where = ContextsTable.COLUMN_NAME+"=?";
+			String[] selectionArgs = new String[1];
 			
-			String[] activate = extras.getStringArray("activate");
-			if (activate != null){
-				values.put(ContextsTable.COLUMN_ACTIVE, 1);
-				for (String a : activate){
-					selectionArgs[0] = a;
-					res.update(GoDoContentProvider.CONTEXTS_URI, values, where, selectionArgs);
+			int max = 4;
+			if (extras != null){
+				String[] deactivate = extras.getStringArray("deactivate");
+				if (deactivate != null){
+					values.put(ContextsTable.COLUMN_ACTIVE, 0);
+					for (String d : deactivate){
+						selectionArgs[0] = d;
+						res.update(GoDoContentProvider.CONTEXTS_URI, values, where, selectionArgs);
+					}
 				}
+				
+				String[] activate = extras.getStringArray("activate");
+				if (activate != null){
+					values.put(ContextsTable.COLUMN_ACTIVE, 1);
+					for (String a : activate){
+						selectionArgs[0] = a;
+						res.update(GoDoContentProvider.CONTEXTS_URI, values, where, selectionArgs);
+					}
+				}
+			
+				max = extras.getInt("max_notify", 4);
 			}
-		
-			max = extras.getInt("max_notify", 4);
+			if (max > 0)
+				context.startService(new Intent(context, NotificationService.class).putExtra("max_notify", max));
+		} else {
+			Log.e("GoDo", "Unknown intent action "+action+", "+intent);
 		}
-		if (max > 0)
-			context.startService(new Intent(context, NotificationService.class).putExtra("max_notify", max));
+		
 	}
 		
 }
