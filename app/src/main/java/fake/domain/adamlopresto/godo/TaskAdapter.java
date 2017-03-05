308047b3a72aa786152c425f93df5e2dfac56b0a
Adam Lopresto <adamlopresto@gmail.com>
2017-03-05 14:22:02 -0600

    Tweak sort order
    A due/plan date without a time is sorted after any particular time. No
    date is after any particular date/time, even date of today but after
    now.

--- app/src/main/java/fake/domain/adamlopresto/godo/TaskAdapter.java
+++ app/src/main/java/fake/domain/adamlopresto/godo/TaskAdapter.java
@@ -26,8 +26,8 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskHolder> im
     public static final String[] PROJECTION = {"_id", "task_name",
             "task_notes", "instance_notes", "due_date", "plan_date", "done_date"};
     public static final String SORT = "done_date is not null, " +
-            "case when due_date <= DATETIME('now', 'localtime') then due_date || ' 23:59:59' else '9999-99-99' end, " +
-            "coalesce(plan_date || ' 00:00:00', DATETIME('now', 'localtime')), " +
+            "case when DATE(due_date) <= DATE('now', 'localtime') then due_date || ' 23:59:59' else '9999-99-99' end, " +
+            "coalesce(plan_date || ' 23:59:58', DATE('now', 'localtime') || ' 23:59:59'), " +
             "due_date || ' 23:59:59', notification DESC, next_steps DESC, random()";
 
     @SuppressWarnings ("UnusedDeclaration")
